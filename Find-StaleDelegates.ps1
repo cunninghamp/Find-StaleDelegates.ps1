71c4eb1cd2ff902a611dd550fff9716296a689ef
Paul Cunningham <paul@locklan.com.au>
2015-01-21 20:03:20 +1000

    Find-StaleDelegates.ps1 added

---
+++ Find-StaleDelegates.ps1
@@ -0,0 +1,69 @@
+﻿<#
+.SYNOPSIS
+Find-StaleDelegates.ps1
+
+.DESCRIPTION 
+Sample script for locating stale delegate entries on Exchange Server 2007 mailboxes.
+
+.EXAMPLE
+.\ScriptTemplate.ps1
+
+.LINK
+https://exchangeserverpro.com
+
+.NOTES
+Written by: Paul Cunningham
+
+For more Exchange Server tips, tricks and news
+check out Exchange Server Pro.
+
+* Website:	http://exchangeserverpro.com
+* Twitter:	http://twitter.com/exchservpro
+
+Find me on:
+
+* My Blog:	http://paulcunningham.me
+* Twitter:	https://twitter.com/paulcunningham
+* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* Github:	https://github.com/cunninghamp
+
+Change Log:
+V1.00, 21/01/2015 - Initial version
+#>
+
+#Requires -Version 2
+
+if (Get-PSSnapin -Name Microsoft.Exchange.Management.PowerShell.Admin -Registered -ErrorAction SilentlyContinue) {$mailboxes = @(Get-MailboxCalendarSettings | Where {$_.ResourceDelegates -ne $null})}
+if (Get-PSSnapin -Name Microsoft.Exchange.Management.PowerShell.E2010 -Registered -ErrorAction SilentlyContinue) {$mailboxes = @(Get-Mailbox | Get-CalendarProcessing | Where {$_.ResourceDelegates -ne $null})}
+
+if ($mailboxes.count -eq 0)
+{
+    Write-Host "No mailboxes with resource delegates found."
+    EXIT
+}
+
+foreach ($mailbox in $mailboxes)
+{
+    
+
+	Write-Host -ForegroundColor White "*** Checking $($mailbox.Identity.Name)"
+
+    $delegates = @($mailbox | Select -ExpandProperty:ResourceDelegates)
+
+    foreach ($delegate in $delegates)
+    {
+        try
+        {
+            $getdelegate = Get-Mailbox $delegate -ErrorAction STOP
+            Write-Host -ForegroundColor Green "$($delegate.Name) is okay"
+        }
+        catch
+        {
+            Write-Host -ForegroundColor Yellow "$($delegate.Name) is stale"
+
+        }
+    }
+
+}
+
+Write-Host -ForegroundColor White "*** Finished"
